0ba14ddbe4b96c051b24fc09ab64ff7333b573f4
Reuben Raj <helix99@gmail.com>
2015-02-15 20:40:20 +0000

    Added tracker facade file

---
+++ CharityTracker/TrackerFacade.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Transactions;
+using System.Web;
+
+namespace CharityTracker
+{
+    public class TrackerFacade
+    {
+        public TrackerFacade()
+        { }
+
+        public List<ActivityType> GetAllActivityTypes()
+        {
+            using (CharityTrackerEntities _context = new CharityTrackerEntities())
+            {
+                return _context.ActivityTypes
+                    .Where(a => a.IsActive == true)
+                    .OrderBy(a => a.ActivityType1)
+                    .ToList<ActivityType>();
+            }
+        }
+
+        public List<vwGetUser> GetAllActiveUsers()
+        {
+            using (CharityTrackerEntities _context = new CharityTrackerEntities())
+            {
+                return _context.vwGetUsers
+                    .Where(u => u.UserId != new Guid("B7D36A7B-B613-4CE6-998B-B22086A0347A"))
+                    .OrderBy(u => u.UserName)
+                    .ToList<vwGetUser>();
+            }
+        }
+
+        public List<Office> GetOffices()
+        {
+            using (CharityTrackerEntities _context = new CharityTrackerEntities())
+            {
+                return _context.Offices
+                    .Where(o => o.IsActive == true)
+                    .ToList<Office>();
+            }
+        }
+
+        public List<vwActivity> GetActivities(Guid userId)
+        {
+            using (CharityTrackerEntities _context = new CharityTrackerEntities())
+            {
+                return _context.vwActivities
+                    .Where(u => u.UserId == userId)
+                    .OrderByDescending(u => u.LastUpdated)
+                    .ToList<vwActivity>();
+            }
+        }
+
+        public List<vwActivitiesByType> GetActivitiesByType()
+        {
+            using (CharityTrackerEntities _context = new CharityTrackerEntities())
+            {
+                return _context.vwActivitiesByTypes.ToList<vwActivitiesByType>();
+            }
+        }
+
+        public List<vwActivitiesByUser> GetActivitiesByUser()
+        {
+            using (CharityTrackerEntities _context = new CharityTrackerEntities())
+            {
+                return _context.vwActivitiesByUsers
+                    .Where(u => u.UserId != new Guid("B7D36A7B-B613-4CE6-998B-B22086A0347A"))
+                    .OrderBy(u => u.UserName)
+                    .ToList<vwActivitiesByUser>();
+            }
+        }
+
+        public List<vwActivitiesByType> GetActivitiesByTypeByUser(Guid userid)
+        {
+            using (CharityTrackerEntities _context = new CharityTrackerEntities())
+            {
+                return _context.vwActivitiesByTypes
+                   .Where(a => a.UserId == userid)
+                   .OrderBy(a => a.ActivityType)
+                   .ToList<vwActivitiesByType>();
+            }
+        }
+        
+        public void AddActivity(Activity activity)
+        {
+            using (CharityTrackerEntities _context = new CharityTrackerEntities())
+            {
+                using (TransactionScope scope = new TransactionScope())
+                {
+                    List<Office> lstOffices = _context.Offices
+                        .Where(o => o.IsActive == true)
+                        .Where(o => o.IsCompleted == false)
+                        .Take(2)
+                        .OrderBy(o => o.ListOrder)
+                        .ToList<Office>();
+
+                    var remainingOfficeMileage = lstOffices[0].Mileage - lstOffices[0].MileageCompleted;
+
+                    if (remainingOfficeMileage >= activity.Mileage)
+                    {
+                        lstOffices[0].MileageCompleted += activity.Mileage;
+                    }
+                    else if (remainingOfficeMileage <= activity.Mileage)
+                    {
+                        var currentMileage = activity.Mileage - remainingOfficeMileage;
+
+                        lstOffices[0].MileageCompleted += remainingOfficeMileage;
+                        lstOffices[0].IsCompleted = true;
+                        lstOffices[1].MileageCompleted += currentMileage;
+                    }
+
+                    _context.Activities.Add(activity);
+                    _context.SaveChanges();
+
+                    scope.Complete();
+                }
+            }
+        }
+    }
+}
--- CharityTracker/obj/Release/Package/PackageTmp/Default.aspx
+++ CharityTracker/obj/Release/Package/PackageTmp/Default.aspx
@@ -2,6 +2,70 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="FeaturedContent" runat="server">
+    <div class="page-header"><h1>Dashboard <small>Let's get a quick overview...</small></h1></div> 
+    <div class="row">            
+        <div class="col-md-2 col-sm-4">
+            <div class="panel panel-default clearfix dashboard-stats rounded">
+                <span class="sparkline transit"><span class="text-red"><asp:Literal ID="litTotalUsersMiles" runat="server"></asp:Literal> of <asp:Literal ID="litTotalOfficeMiles" runat="server"></asp:Literal></span></span>
+                <i class="fa fa-circle bg-success transit stats-icon"></i>
+                <h3 class="transit"><asp:Literal ID="litTotalMileage" runat="server"></asp:Literal> <small class="text-green"><%--<i class="fa fa-caret-up"></i>--%> </small></h3>
+                <p class="text-muted transit">Total Mileage</p>
+            </div>
+        </div>               
+    </div>
+    <div class="row">                
+        <asp:Repeater ID="ActivityOffices" runat="server">
+            <ItemTemplate>
+                <div class="col-md-2 col-sm-4">
+                    <div class="panel panel-default clearfix dashboard-stats rounded">
+                    <span class="sparkline transit"><span class="text-red"><%# Eval("MileageCompleted")%> of <%# Eval("Mileage")%></span></span>
+                    <i class="fa fa-university bg-info transit stats-icon"></i>
+                    <h3 class="transit"><%# Eval("Mileage")%> <small class="text-red"></small></h3>
+                    <p class="text-muted transit"><%# Eval("Office1")%> Office</p>
+                    </div>
+                </div>
+            </ItemTemplate>
+        </asp:Repeater>
+    </div>
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="row">
+        <div class="col-lg-12">                
+            <div class="panel panel-default">
+                <div class="panel-heading clean">Total Activities<div class="pull-right"></div></div>
+                <div class="panel-body">
+                    <div>
+                        <asp:Repeater ID="ActivitiesRepeater" runat="server">
+                            <HeaderTemplate>
+                                <table class="table no-margn">
+                                <thead>
+                                <tr>
+                                <th>#</th>
+                                <th>Name</th>
+                                <th>Total Mileage</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                            </HeaderTemplate>
+                            <ItemTemplate>
+                                <tr class="active">
+                                <td><%# Eval("RowNumber")%></td>
+                                <td><%# Eval("Username")%></td>
+                                <td><%# Eval("TotalMileage")%></td>
+                                </tr>
+                            </ItemTemplate>
+                            <AlternatingItemTemplate>
+                                <tr class="warning">
+                                <td><%# Eval("RowNumber")%></td>
+                                <td><%# Eval("Username")%></td>
+                                <td><%# Eval("TotalMileage")%></td>
+                                </tr>
+                            </AlternatingItemTemplate>
+                            <FooterTemplate></tbody></table></FooterTemplate>
+                        </asp:Repeater>
+                    </div>
+                </div>
+            </div>
+        </div>               
+    </div> 
 </asp:Content>
