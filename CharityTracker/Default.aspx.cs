64120a9fd86967dfa769186bbf0b4d199dc23e10
Reuben Raj <helix99@gmail.com>
2015-02-15 20:41:32 +0000

    Update to default.aspx code behind file

--- CharityTracker/Default.aspx.cs
+++ CharityTracker/Default.aspx.cs
@@ -9,9 +9,46 @@ namespace CharityTracker
 {
     public partial class Default : System.Web.UI.Page
     {
+        private const string USER_LIST = "USER_LIST";
+        private const string ACTIVITY_TYPE_LIST = "ACTIVITY_TYPE_LIST";
+
+        private TrackerFacade _facade = new TrackerFacade();
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Page.IsPostBack)
+            {
+                SetupForm();
+            }
+        }
 
+        private void SetupForm()
+        {
+            FillOffices();
+            FillActivitiesByUser();
+        }
+
+        private void FillOffices()
+        {
+            List<Office> lstOffices = _facade.GetOffices();
+            ActivityOffices.DataSource = lstOffices;
+            ActivityOffices.DataBind();
+
+            litTotalOfficeMiles.Text = lstOffices.Sum(o => o.Mileage).ToString();
+        }
+
+        private void FillActivitiesByUser()
+        {
+            List<vwActivitiesByUser> lstActivitiesByUser = _facade.GetActivitiesByUser();
+            ActivitiesRepeater.DataSource = lstActivitiesByUser;
+            ActivitiesRepeater.DataBind();
+
+            litTotalMileage.Text = lstActivitiesByUser.Sum(s => s.TotalMileage).ToString();
+            litTotalUsersMiles.Text = litTotalMileage.Text;
         }
     }
 }
